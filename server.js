2906f2025169346e7e4401e7d87550a9894b02cf
IndraCahya123 <indracahya51@gmail.com>
2021-03-29 07:03:08 +0700

    back-end first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+
+const cors = require("cors");
+
+const router = require("./src/routes");
+
+const portServer = 8000;
+
+//body parser
+app.use(express.json());
+
+app.use(cors());
+
+//routes group
+app.use("/api/v1", router);
+app.use("/uploadedImages", express.static("uploadedImages"));
+
+app.listen(portServer, () => console.log(`your server is running at port : ${portServer}`));
+
+//root url = localhost:3000/api/v1
